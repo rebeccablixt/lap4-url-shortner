e2c8b16e5b10a26f54eb5a9b316f351758d13ecb
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-07-12 18:34:30 +0200

    add url_input. add routes to app.py. add randomising functionality to a range of 6 letter

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect
 from flask_cors import CORS
 from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
+import string
+import random
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    if request.method == 'POST':
+        new_url = {"longUrl": request.form['url_input']}
+        print(new_url)
+
+        letters = string.ascii_letters
+        urlend = ''.join(random.choice(letters) for i in range(6))
+        print(urlend)
+        return render_template("index.html")
+
+    if request.method == 'GET':
+        return render_template("index.html")
+
+
+@app.route('/<urlend>')
+def show(urlend):
+    # db query
+    return redirect('urlend')
+
+    # add new_url & urlend to db
