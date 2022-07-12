319fa7ebbcfca6a83756222b1994b4c9b8aa2f24
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-07-12 17:57:22 +0200

    basic setup

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return f"hello world"
